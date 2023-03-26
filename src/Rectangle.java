4c2f15849799970a6b1022a6d164184f5f8ded02
AlexandrStopkin <stnalex1@gmail.com>
2023-03-26 10:31:36 +0200

    Add constructor, getters and setters, toString(), equals(), Hashcode()

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 //Задача 1.
 // Создать класс Rectangle (прямоугольник), которому в конструкторе нужно передавать длину и ширину.
 //
@@ -5,5 +7,53 @@
 //
 // Написать тесты для этого компаратора.
 public class Rectangle {
+  private int sideA;
+  private int sideB;
 
+  public int getSideA() {
+    return sideA;
+  }
+
+  public void setSideA(int sideA) {
+    this.sideA = sideA;
+  }
+
+  public int getSideB() {
+    return sideB;
+  }
+
+  public void setSideB(int sideB) {
+    this.sideB = sideB;
+  }
+
+  public Rectangle(int sideA, int sideB) {
+    this.sideA = sideA;
+    this.sideA = sideB;
+  }
+
+  @Override
+  public String toString() {
+    return "Rectangle{" +
+        "sideA=" + sideA +
+        ", sideB=" + sideB +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Rectangle rectangle = (Rectangle) o;
+    return (sideA == rectangle.sideA && sideB == rectangle.sideB) ||
+        (sideA == rectangle.sideB && sideB == rectangle.sideA);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(sideA, sideB);
+  }
 }
